ccaad53350cb8a702dfab54a7005f7276c670ecb
Anish <anishpal9621@gmail.com>
2023-03-25 21:07:40 +0530

    2nd attempt of assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,12 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         B obj=new B();
-        System.out.println(obj.meth());
+        System.out.println(obj.meth("Initial call"));
         System.out.println(obj.meth());
     }
 }
 class A{
-    public String meth(){
+    public String meth(String str){
         return "Invoking method from class A";
     }
 }
