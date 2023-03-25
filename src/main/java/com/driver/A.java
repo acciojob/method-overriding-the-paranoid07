1304959cc53142120f7d0faee25d87fb87639823
Anish <anishpal9621@gmail.com>
2023-03-25 21:12:01 +0530

    3rd attempt

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class A {
+    public String meth(String str){
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,17 +5,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         B obj=new B();
-        System.out.println(obj.meth("Initial call"));
+        System.out.println(obj.meth("First call"));
         System.out.println(obj.meth());
-    }
-}
-class A{
-    public String meth(String str){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
     }
 }
