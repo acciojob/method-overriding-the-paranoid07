d19668c83584881384a7b31c48f300e159d81b0f
Anish <anishpal9621@gmail.com>
2023-03-25 21:03:03 +0530

    Initial attempt of assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj=new B();
+        System.out.println(obj.meth());
+        System.out.println(obj.meth());
+    }
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
